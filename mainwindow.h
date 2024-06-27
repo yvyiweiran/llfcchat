c18d0ee1fb4a206b16df9960de4bb1cd4aff60b2
yvyiweiran <3423028697@qq.com>
2024-06-27 16:45:41 +0800

    添加注释

--- mainwindow.h
+++ mainwindow.h
@@ -1,3 +1,13 @@
+
+/**
+ * @file        mainwindow.h
+ * @brief       主窗口头文件
+ *
+ * @author      羽衣未染
+ * @date        2024/06/27
+ * @history
+ */
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
@@ -9,11 +19,18 @@ class MainWindow;
 }
 QT_END_NAMESPACE
 
+/**
+ * @brief 主窗口类
+ */
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief       构造函数
+     * @param[in]   parent 父对象指针
+     */
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
